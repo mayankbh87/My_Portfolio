e78faadf027476180d4ff9154a0347272f88dd9c
Mayank Bhardwaj <mayank.bh87@gmail.com>
2020-08-20 18:02:48 +0530

    Updates

---
+++ precache-manifest.9c38c97889ba4352cde6906ce3e2f9ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3c3effff8d71d89766eaf65d43ce0d",
+    "url": "/My_Portfolio/index.html"
+  },
+  {
+    "revision": "92dfe763746f2333c91d",
+    "url": "/My_Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9842830796f504c5d1e",
+    "url": "/My_Portfolio/static/js/2.0556c11d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My_Portfolio/static/js/2.0556c11d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92dfe763746f2333c91d",
+    "url": "/My_Portfolio/static/js/main.11943680.chunk.js"
+  },
+  {
+    "revision": "eb8029e7e4405da47f9a",
+    "url": "/My_Portfolio/static/js/runtime-main.5b24b71c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/My_Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
